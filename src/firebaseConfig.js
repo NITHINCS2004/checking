37edc96ca150ecc04ce166e413725078cfa6ac98
123456NI-GIT <157794860+123456NI-GIT@users.noreply.github.com>
2025-03-03 15:38:13 +0530

    changes

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -14,19 +14,18 @@ const firebaseConfig = {
   export const db=firebase.firestore(app)*/
 
   // firebaseConfig.js
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+  import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAC-ctJEZ82MtRQhokgtO3JE-QN-Ggd2eM",
-    authDomain: "gen-lang-client-0724194648.firebaseapp.com",
-    projectId: "gen-lang-client-0724194648",
-    storageBucket: "gen-lang-client-0724194648.appspot.com", // Fixed incorrect storage bucket
-    messagingSenderId: "486340617177",
-    appId: "1:486340617177:web:6d3b11fc0888f95691efc8",
-    measurementId: "G-6XKYG227WP"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  export default app;
+  
